7dce4cdd1181bfee17e79361188e0a87fb394c94
Jéssica Santos <39637921+Jesslss@users.noreply.github.com>
2024-02-02 18:16:07 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print(f"O resultado da soma de {num1} e {num2} é igual a: {soma}")
